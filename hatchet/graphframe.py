0b6725ed49b145aa283d45b4f0a12543a4140e97
Matthew Kotila <matthew.r.kotila@gmail.com>
2018-03-27 17:40:52 -0700

    pr comments addressed

--- hatchet/graphframe.py
+++ hatchet/graphframe.py
@@ -12,8 +12,6 @@
 
 from hpctoolkit_reader import HPCToolkitReader
 from caliper_reader import CaliperReader
-from node import Node
-from graph import Graph
 import pandas as pd
 
 class GraphFrame:
@@ -21,8 +19,6 @@ class GraphFrame:
     """
 
     def __init__(self):
-        self.node_column_name = 'node'
-        self.rank_column_name = 'mpi.rank'
         self.num_pes = 0
         self.num_nodes = 0
         self.num_metrics = 0
@@ -42,18 +38,15 @@ class GraphFrame:
 
         (self.graph, self.dataframe) = reader.create_graph()
 
-    def filter(self, is_row_qualified, has_descendants=False):
+    def filter(self, is_row_qualified):
         """Filters self's dataframe.
 
-        Gets rows from self's dataframe that qualify, and possibly rows of
-        descendant nodes of qualifying nodes and returns a new graphframe
-        with qualified dataframe rows and original graph.
+        Creates and returns a new graphframe whose dataframe is a copied subset
+        of self's dataframe and whose graph is self's graph.
 
         Args:
             is_row_qualified (:obj:`function`): Function that returns True or
                 False if a pandas.Series qualifies.
-            has_descendants (bool): Whether the filter keeps descendants of
-                matched rows.
 
         Returns:
             A new graphframe that is the result of filtering self.
@@ -61,33 +54,26 @@ class GraphFrame:
         Raises:
             ValueError: When an argument is invalid.
         """
+        # currently necessary hard-coded values
+        self.node_column_name = 'node'
+
+        # check for invalid arguments
         if not callable(is_row_qualified):
             raise ValueError('is_row_qualified is not a function.')
 
-        if not isinstance(has_descendants, bool):
-            raise ValueError('has_descendants is not a bool.')
+        # get qualified rows
+        qualified_rows = self.dataframe.apply(is_row_qualified, axis=1)
 
-        # get boolean series for is_qualified rows
-        apply_series = self.dataframe.apply(is_row_qualified, axis=1)
+        # construct copied subset dataframe
+        copied_subset_dataframe = self.dataframe.loc[qualified_rows].copy()
+        copied_subset_dataframe.reset_index(drop=True, inplace=True)
+        indices = list(self.dataframe.index.names)
+        if self.node_column_name in indices:
+            indices.insert(0, indices.pop(indices.index(self.node_column_name)))
+        copied_subset_dataframe.set_index(indices, drop=False, inplace=True)
 
-        # get indices for True/qualified rows
-        indices = set(self.dataframe.loc[apply_series, self.node_column_name])
-
-        # add indices of descendants if has_descendants is True
-        if has_descendants is True:
-            for node in indices.copy():
-                for descendant in node.traverse():
-                    indices.add(descendant)
-
-        # construct filtered dataframe
-        filtered_dataframe = self.dataframe.loc[list(indices)].copy()
-        filtered_dataframe.reset_index(drop=True, inplace=True)
-        filtered_dataframe.set_index([self.node_column_name,
-                                      self.rank_column_name],
-                                     drop=False, inplace=True)
-
-        # return new graphframe with copy of filtered dataframe and graph
+        # construct filtered graphframe
         filtered_graphframe = GraphFrame()
-        filtered_graphframe.dataframe = filtered_dataframe
+        filtered_graphframe.dataframe = copied_subset_dataframe
         filtered_graphframe.graph = self.graph
         return filtered_graphframe
--- tests/graphframe_test.py
+++ tests/graphframe_test.py
@@ -11,119 +11,98 @@
 ##############################################################################
 
 import pandas as pd
-from hatchet import *
+from hatchet.node import Node
+from hatchet.graph import Graph
+from hatchet.graphframe import GraphFrame
 
 
-def test_filter_without_descendants():
+def test_filter_a():
     """Sanity test GraphFrame.filter()."""
-    # hard-coded graph
-    nodes = [Node(callpath_tuple=None, parent=None) for _ in range(7)]
-    nodes[0].callpath = ('main',)
-    nodes[0].parent = None
-    nodes[0].children = [nodes[1], nodes[4]]
-    nodes[1].callpath = ('main', 'MPI_Send')
-    nodes[1].parent = nodes[0]
-    nodes[1].children = [nodes[2], nodes[3]]
-    nodes[2].callpath = ('main', 'MPI_Send', 'descendant_a')
-    nodes[2].parent = nodes[1]
-    nodes[2].children = []
-    nodes[3].callpath = ('main', 'MPI_Send', 'descendant_b')
-    nodes[3].parent = nodes[1]
-    nodes[3].children = []
-    nodes[4].callpath = ('main', 'MPI_Recv')
-    nodes[4].parent = nodes[0]
-    nodes[4].children = [nodes[5]]
-    nodes[5].callpath = ('main', 'MPI_Recv', 'descendant_a')
-    nodes[5].parent = nodes[4]
-    nodes[5].children = [nodes[6]]
-    nodes[6].callpath = ('main', 'MPI_Recv', 'descendant_a', 'descendant_a_a')
-    nodes[6].parent = nodes[5]
-    nodes[6].children = []
-    graph = Graph(roots=None)
-    graph.roots = [nodes[0]]
+    # graphframe input
+    input_nodes = [Node(callpath_tuple=None, parent=None) for _ in range(3)]
+    input_nodes[0].callpath = ('main',)
+    input_nodes[0].parent = None
+    input_nodes[0].children = [input_nodes[1], input_nodes[2]]
+    input_nodes[1].callpath = ('main', 'MPI_Send')
+    input_nodes[1].parent = input_nodes[0]
+    input_nodes[1].children = []
+    input_nodes[2].callpath = ('main', 'MPI_Recv')
+    input_nodes[2].parent = input_nodes[0]
+    input_nodes[2].children = []
+    input_graph = Graph(roots=None)
+    input_graph.roots = [input_nodes[0]]
+    input_data = [[1, 0, 'main', input_nodes[0]],
+                  [3023, 0, 'MPI_Send', input_nodes[1]],
+                  [8236, 0, 'MPI_Recv', input_nodes[2]]]
+    input_columns = ['count', 'mpi.rank', 'path', 'node']
+    input_dataframe = pd.DataFrame(data=input_data, columns=input_columns)
+    input_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    input_graphframe = GraphFrame()
+    input_graphframe.dataframe = input_dataframe
+    input_graphframe.graph = input_graph
 
-    # hard-coded dataframe
-    data = [[1, 0, 'main', nodes[0]],
-            [23, 0, 'MPI_Send', nodes[1]],
-            [8236, 0, 'descendant_a', nodes[2]],
-            [131, 0, 'descendant_b', nodes[3]],
-            [4452, 0, 'descendant_a_a', nodes[6]]]
-    columns = ['count', 'mpi.rank', 'path', 'node']
-    dataframe = pd.DataFrame(data=data, columns=columns)
-    dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    # expected graphframe output
+    expected_graph = input_graph
+    expected_data = [[3023, 0, 'MPI_Send', input_nodes[1]],
+                     [8236, 0, 'MPI_Recv', input_nodes[2]]]
+    expected_columns = ['count', 'mpi.rank', 'path', 'node']
+    expected_dataframe = pd.DataFrame(data=expected_data,
+                                      columns=expected_columns)
+    expected_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    expected_graphframe = GraphFrame()
+    expected_graphframe.dataframe = expected_dataframe
+    expected_graphframe.graph = expected_graph
 
-    # hard-coded graphframe
-    graphframe = GraphFrame()
-    graphframe.dataframe = dataframe
-    graphframe.graph = graph
+    # computed graphframe output
+    is_row_qualified = lambda x: 'MPI' in x['path']
+    computed_graphframe = input_graphframe.filter(is_row_qualified)
 
-    # filter the graphframe
-    result = graphframe.filter(lambda x: 'MPI' in x['path'])
-
-    # hard-coded result dataframe
-    result_data = [[23, 0, 'MPI_Send', nodes[1]]]
-    result_columns = ['count', 'mpi.rank', 'path', 'node']
-    result_dataframe = pd.DataFrame(data=result_data, columns=result_columns)
-    result_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
-
-    # test dataframe equalities
-    assert result.dataframe.equals(result_dataframe)
+    # test graphframe equalities
+    assert computed_graphframe.dataframe.equals(expected_graphframe.dataframe)
+    assert computed_graphframe.graph is expected_graphframe.graph
 
 
-def test_filter_with_descendants():
+def test_filter_b():
     """Sanity test GraphFrame.filter()."""
-    # hard-coded graph
-    nodes = [Node(callpath_tuple=None, parent=None) for _ in range(7)]
-    nodes[0].callpath = ('main',)
-    nodes[0].parent = None
-    nodes[0].children = [nodes[1], nodes[4]]
-    nodes[1].callpath = ('main', 'MPI_Send')
-    nodes[1].parent = nodes[0]
-    nodes[1].children = [nodes[2], nodes[3]]
-    nodes[2].callpath = ('main', 'MPI_Send', 'descendant_a')
-    nodes[2].parent = nodes[1]
-    nodes[2].children = []
-    nodes[3].callpath = ('main', 'MPI_Send', 'descendant_b')
-    nodes[3].parent = nodes[1]
-    nodes[3].children = []
-    nodes[4].callpath = ('main', 'MPI_Recv')
-    nodes[4].parent = nodes[0]
-    nodes[4].children = [nodes[5]]
-    nodes[5].callpath = ('main', 'MPI_Recv', 'descendant_a')
-    nodes[5].parent = nodes[4]
-    nodes[5].children = [nodes[6]]
-    nodes[6].callpath = ('main', 'MPI_Recv', 'descendant_a', 'descendant_a_a')
-    nodes[6].parent = nodes[5]
-    nodes[6].children = []
-    graph = Graph(roots=None)
-    graph.roots = [nodes[0]]
+    # graphframe input
+    input_nodes = [Node(callpath_tuple=None, parent=None) for _ in range(3)]
+    input_nodes[0].callpath = ('MPI_call_a',)
+    input_nodes[0].parent = None
+    input_nodes[0].children = [input_nodes[1]]
+    input_nodes[1].callpath = ('MPI_call_a', 'intermediary_call')
+    input_nodes[1].parent = input_nodes[0]
+    input_nodes[1].children = [input_nodes[2]]
+    input_nodes[2].callpath = ('MPI_call_a', 'intermediary_call', 'MPI_call_b')
+    input_nodes[2].parent = input_nodes[1]
+    input_nodes[2].children = []
+    input_graph = Graph(roots=None)
+    input_graph.roots = [input_nodes[0]]
+    input_data = [[1, 0, 'MPI_call_a', input_nodes[0]],
+            [3023, 0, 'intermediary_call', input_nodes[1]],
+            [9274, 0, 'MPI_call_b', input_nodes[2]]]
+    input_columns = ['count', 'mpi.rank', 'path', 'node']
+    input_dataframe = pd.DataFrame(data=input_data, columns=input_columns)
+    input_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    input_graphframe = GraphFrame()
+    input_graphframe.dataframe = input_dataframe
+    input_graphframe.graph = input_graph
 
-    # hard-coded dataframe
-    data = [[1, 0, 'main', nodes[0]],
-            [23, 0, 'MPI_Send', nodes[1]],
-            [8236, 0, 'descendant_a', nodes[2]],
-            [131, 0, 'descendant_b', nodes[3]],
-            [4452, 0, 'descendant_a_a', nodes[6]]]
-    columns = ['count', 'mpi.rank', 'path', 'node']
-    dataframe = pd.DataFrame(data=data, columns=columns)
-    dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    # expected graphframe output
+    expected_graph = input_graph
+    expected_data = [[1, 0, 'MPI_call_a', input_nodes[0]],
+                     [9274, 0, 'MPI_call_b', input_nodes[2]]]
+    expected_columns = ['count', 'mpi.rank', 'path', 'node']
+    expected_dataframe = pd.DataFrame(data=expected_data,
+                                      columns=expected_columns)
+    expected_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
+    expected_graphframe = GraphFrame()
+    expected_graphframe.dataframe = expected_dataframe
+    expected_graphframe.graph = expected_graph
 
-    # hard-coded graphframe
-    graphframe = GraphFrame()
-    graphframe.dataframe = dataframe
-    graphframe.graph = graph
+    # computed graphframe output
+    is_row_qualified = lambda x: 'MPI' in x['path']
+    computed_graphframe = input_graphframe.filter(is_row_qualified)
 
-    # filter the graphframe
-    result = graphframe.filter(lambda x: 'MPI' in x['path'],
-                               has_descendants=True)
-
-    # hard-coded result dataframe
-    result_data = [[23, 0, 'MPI_Send', nodes[1]],
-                   [8236, 0, 'descendant_a', nodes[2]],
-                   [131, 0, 'descendant_b', nodes[3]]]
-    result_columns = ['count', 'mpi.rank', 'path', 'node']
-    result_dataframe = pd.DataFrame(data=result_data, columns=result_columns)
-    result_dataframe.set_index(['node', 'mpi.rank'], drop=False, inplace=True)
-
-    # test dataframe equalities
-    assert result.dataframe.equals(result_dataframe)
+    # test graphframe equalities
+    assert computed_graphframe.dataframe.equals(expected_graphframe.dataframe)
+    assert computed_graphframe.graph is expected_graphframe.graph
