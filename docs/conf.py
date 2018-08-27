72b02b179842db56de775abfb7857f78d0a7d46a
Abhinav Bhatele <bhatele@llnl.gov>
2018-08-27 14:49:45 -0700

    add copyright header

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
+
+##############################################################################
+# Copyright (c) 2017-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
 #
+# This file is part of Hatchet.
+# Created by Abhinav Bhatele <bhatele@llnl.gov>.
+# LLNL-CODE-741008. All rights reserved.
+#
+# For details, see: https://github.com/LLNL/hatchet
+# Please also read the LICENSE file for the MIT License notice.
+##############################################################################
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file does only contain a selection of the most common options. For a
