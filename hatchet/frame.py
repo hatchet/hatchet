db024bc771ddf2b99a321a6c24f5312d23e39850
Todd Gamblin <tgamblin@llnl.gov>
2019-08-14 16:13:38 -0600

    frame: Add accessor methods and tests
    - add copy(), __getitem__(), get(), and values() methods for Frame
    - add tests to Frame

--- hatchet/frame.py
+++ hatchet/frame.py
@@ -21,8 +21,35 @@ class Frame:
        attrs (dict): Dictionary of attributes and values.
     """
 
-    def __init__(self, attrs_dict):
-        self.attrs = attrs_dict
+    def __init__(self, attrs=None, **kwargs):
+        """Construt a frame from a dictionary, or from immediate kwargs.
+
+        Arguments:
+            attrs (dict, optional): dictionary of attributes for this
+                Frame.
+
+        Keyword arguments are optional, but if they are provided, they
+        will be used to update the dictionary.  Keys in kwargs take
+        precedence over anything in the attrs dictionary.
+
+        So, these are all functionally equivalent::
+
+            Frame({"name": "foo", "file": "bar.c"})
+            Frame(name="foo", file="bar.c")
+            Frame({"name": "foo"}, file="bar.c")
+            Frame({"name": "foo", "file": "baz.h"}, file="bar.c")
+
+        """
+        # attributes dictionary
+        self.attrs = attrs if attrs else {}
+
+        # add keyword arguments, if any.
+        if kwargs:
+            self.attrs.update(kwargs)
+
+        if not self.attrs:
+            raise ValueError("Frame must be constructed with attributes!")
+
         self._tuple_repr = None
 
     def __eq__(self, other):
@@ -46,3 +73,19 @@ class Frame:
         if not self._tuple_repr:
             self._tuple_repr = tuple((k, self.attrs[k]) for k in sorted(self.attrs))
         return self._tuple_repr
+
+    def copy(self):
+        return Frame(self.attrs.copy())
+
+    def __getitem__(self, name):
+        return self.attrs[name]
+
+    def get(self, name, default=None):
+        return self.attrs.get(name, default)
+
+    def values(self, names):
+        """Return a tuple of attribute values from this Frame."""
+        if isinstance(names, (list, tuple)):
+            return tuple(self.attrs.get(name) for name in names)
+        else:
+            return self.attrs.get(names)
