6e83121f86b3220f8f25e27f51bbcb4afe22aa01
Todd Gamblin <tgamblin@llnl.gov>
2019-08-14 17:29:46 -0700

    node: fix traverse(), add from_lists(), add tests
    - fix Node.traverse() to handle DAGs
    
    - traverse() can optionally extract values from Frames by key (makes
      testing easier)
    
    - add from_lists() to quickly construct Node trees/DAGs with different
      topologies.
    
    - add tests to exercise traverse() and other node methods.

---
+++ hatchet/tests/node.py
@@ -0,0 +1,53 @@
+##############################################################################
+# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Hatchet.
+# Created by Abhinav Bhatele <bhatele@llnl.gov>.
+# LLNL-CODE-741008. All rights reserved.
+#
+# For details, see: https://github.com/LLNL/hatchet
+# Please also read the LICENSE file for the MIT License notice.
+##############################################################################
+
+from hatchet.node import Node
+from hatchet.frame import Frame
+
+
+def test_from_lists():
+    node = Node.from_lists("a")
+    assert node.frame == Frame(name="a")
+
+    a = Frame(name="a")
+    b = Frame(name="b")
+    c = Frame(name="c")
+
+    node = Node.from_lists(["a", ["b", "c"]])
+
+    assert node.frame == a
+    assert node.children[0].frame == b
+    assert node.children[0].children[0].frame == c
+
+
+def test_traverse_pre():
+    node = Node.from_lists(["a", ["b", "d", "e"], ["c", "f", "g"]])
+    assert list(node.traverse(attrs="name")) == ["a", "b", "d", "e", "c", "f", "g"]
+
+
+def test_traverse_post():
+    node = Node.from_lists(["a", ["b", "d", "e"], ["c", "f", "g"]])
+    assert list(node.traverse(order="post", attrs="name")) == [
+        "d",
+        "e",
+        "b",
+        "f",
+        "g",
+        "c",
+        "a",
+    ]
+
+
+def test_traverse_dag():
+    d = Node(Frame(name="d"))
+    node = Node.from_lists(["a", ["b", d], ["c", d]])
+    assert list(node.traverse(attrs="name")) == ["a", "b", "d", "c"]
