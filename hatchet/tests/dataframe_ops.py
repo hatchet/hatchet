d90e253f7bfa7f27d245f7035a2703874beec86e
Stephanie Brink <brink2@llnl.gov>
2019-08-19 13:07:07 -0700

    fix graphframe sub/add operations (fixes #46)
    - unify self and other before operation
    - update only those numeric columns shared between self and other
    - add sub/add tests, check that graph unify did not modify the graph before
      performing operation

--- hatchet/tests/dataframe_ops.py
+++ hatchet/tests/dataframe_ops.py
@@ -18,3 +18,144 @@ def test_filter(mock_graph_literal):
     filtered_gf = gf.filter(lambda x: x["name"].startswith("g"))
     assert len(filtered_gf.dataframe) == 7
     assert all(name.startswith("g") for name in filtered_gf.dataframe["name"])
+
+
+def test_add(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf3 = gf1.add(gf2)
+
+    assert gf3.graph == gf1.graph.union(gf2.graph)
+
+    assert gf3.dataframe["time"].sum() == 330
+    assert gf3.dataframe["time (inc)"].sum() == 1280
+
+    gf4 = gf3.copy()
+    assert gf4.graph == gf3.graph
+
+    gf5 = gf3.add(gf4)
+    assert gf5.graph == gf3.graph == gf4.graph
+
+
+def test_sub(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf3 = gf1.sub(gf2)
+
+    assert gf3.graph == gf1.graph.union(gf2.graph)
+
+    for metric in gf3.exc_metrics + gf3.inc_metrics:
+        assert gf3.dataframe[metric].sum() == 0
+
+    gf4 = gf3.copy()
+    assert gf4.graph == gf3.graph
+
+    gf5 = gf3.sub(gf4)
+    assert gf5.graph == gf3.graph == gf4.graph
+
+
+def test_add_operator(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf3 = gf1 + gf2
+
+    assert gf3.graph == gf1.graph.union(gf2.graph)
+
+    assert gf3.dataframe["time"].sum() == 330
+    assert gf3.dataframe["time (inc)"].sum() == 1280
+
+    gf4 = gf3.copy()
+    assert gf4.graph == gf3.graph
+
+    gf5 = gf3 + gf4
+    assert gf5.graph == gf3.graph == gf4.graph
+
+
+def test_sub_operator(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf3 = gf1 - gf2
+
+    assert gf3.graph == gf1.graph.union(gf2.graph)
+
+    for metric in gf3.exc_metrics + gf3.inc_metrics:
+        assert gf3.dataframe[metric].sum() == 0
+
+    gf4 = gf3.copy()
+    assert gf4.graph == gf3.graph
+
+    gf5 = gf3.sub(gf4)
+
+    assert gf5.graph == gf3.graph == gf4.graph
+
+
+def test_iadd_operator(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf1 += gf2
+
+    assert gf1.graph == gf1.graph.union(gf2.graph)
+
+    assert gf1.dataframe["time"].sum() == 330
+    assert gf1.dataframe["time (inc)"].sum() == 1280
+
+    gf3 = gf1.copy()
+    assert gf3.graph == gf1.graph
+
+    gf3 += gf1
+
+    assert gf3.graph == gf1.graph
+
+
+def test_isub_operator(mock_graph_literal):
+    gf1 = GraphFrame()
+    gf1.from_literal(mock_graph_literal)
+
+    gf2 = GraphFrame()
+    gf2.from_literal(mock_graph_literal)
+
+    assert gf1.graph is not gf2.graph
+
+    gf1 -= gf2
+
+    assert gf1.graph == gf1.graph.union(gf2.graph)
+
+    for metric in gf1.exc_metrics + gf1.inc_metrics:
+        assert gf1.dataframe[metric].sum() == 0
+
+    gf3 = gf1.copy()
+    assert gf3.graph == gf1.graph
+
+    gf3 -= gf1
+
+    assert gf3.graph == gf1.graph
