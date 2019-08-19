84a7b3c0fc13c5f6e024ee62744156c265c857fe
Abhinav Bhatele <bhatele@cs.umd.edu>
2019-08-18 23:51:22 -0400

    test for drop_index_levels

--- hatchet/tests/graphframe_ops.py
+++ hatchet/tests/graphframe_ops.py
@@ -19,3 +19,14 @@ def test_copy(mock_graph_literal):
     other = gf.copy()
 
     assert gf.graph == other.graph
+
+
+def test_drop_index_levels(calc_pi_hpct_db):
+    gf = GraphFrame()
+    gf.from_hpctoolkit(str(calc_pi_hpct_db))
+    num_nodes = len(gf.graph)
+
+    gf.drop_index_levels()
+    num_rows = len(gf.dataframe.index)
+
+    assert num_nodes == num_rows
