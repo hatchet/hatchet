4b33413c15f898cf920cc8b07a659be637cd4588
Abhinav Bhatele <bhatele@cs.umd.edu>
2020-01-09 23:06:53 -0500

    update copyright year (#116)

--- docs/examples.rst
+++ docs/examples.rst
@@ -1,3 +1,8 @@
+.. Copyright 2019-2020 University of Maryland and other Hatchet Project
+   Developers. See the top-level LICENSE file for details.
+
+   SPDX-License-Identifier: MIT
+
 Analysis Examples
 =================
 
--- examples/time-read.py
+++ examples/time-read.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2017-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2017-2020 Lawrence Livermore National Security, LLC and other
 # Hatchet Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
