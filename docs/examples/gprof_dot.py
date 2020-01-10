c53ca8629df20af0b7ecc3d8ce2751027d762059
Abhinav Bhatele <bhatele@cs.umd.edu>
2020-01-09 23:22:33 -0500

    add simple examples section to doc

--- docs/examples/gprof_dot.py
+++ docs/examples/gprof_dot.py
@@ -7,17 +7,12 @@
 
 from __future__ import print_function
 
-import pandas as pd
-
 import hatchet as ht
-
-pd.set_option("display.width", 500)
-pd.set_option("display.max_colwidth", 30)
 
 
 if __name__ == "__main__":
     gf = ht.GraphFrame.from_gprof_dot(
-        "hatchet/tests/data/gprof2dot-cpi/callgrind.dot.64042.0.1"
+        "../../hatchet/tests/data/gprof2dot-cpi/callgrind.dot.64042.0.1"
     )
 
     print(gf.dataframe)
