350fb1dd6c7c9ffccd9969db3829481dcfc361dd
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2020-07-21 18:15:27 -0700

    Moved description comments into functions.

--- hatchet/util/profiler.py
+++ hatchet/util/profiler.py
@@ -64,29 +64,70 @@ class Timer(object):
 class Profiler:
     def __init__(self, prf=cProfile.Profile()):
         self.prf = prf
+        self.running_total = 0
 
     def start(self):
+        """
+            Description: Place before the block of code to be profiled.
+        """
         self.prf.enable()
 
     def end(self):
+        """
+            Description: Place at the end of the block of code being profiled.
+            Return: The total runtime of the last profile attempt
+        """
         self.prf.disable()
+        return self.getLastRuntime()
 
     def reset(self):
+        """
+            Description: If profiling across multiple different files or different
+            functionalities in one program run, this must be called between
+            profile instances to clear out data from prior start-stop block.
+        """
         self.prf = cProfile.Profile()
+        self.running_total = 0
+
+    def getLastRuntime(self):
+        """
+            Description: Retrieves the last runtime from a sequence of profiles if
+            profiling in a loop to get average statistics.
+        """
+        last = pstats.Stats(self.prf).__dict__["total_tt"] - self.running_total
+        self.running_total += last
+        return last
 
     def getRuntime(self):
+        """
+            Description: Get the total cumulative runtime logged by this profilier.
+            Reset by calls to .reset().
+        """
         return pstats.Stats(self.prf).__dict__["total_tt"]
 
     def getStats(self):
+        """
+            Description: Get stats as a pstats output.
+        """
         s = io.StringIO()
         return pstats.Stats(self.prf, stream=s)
 
     def getAverageRuntime(self, num_of_runs):
+        """
+            Description: Get average runtime over a number of trials. Takes the cumulative
+            runtime and divides by the number of trials attempted.
+        """
         return pstats.Stats(self.prf).__dict__["total_tt"] / num_of_runs
 
     # sorting options
     # 'calls', 'cumulative', 'filename', 'ncalls', 'pcalls', 'line', 'name', 'nfl' (name file line), 'stdname', 'time'
-    def dumpSortedStats(self, sortby, filename):
+    def dumpSortedStats(self, sortby="cumulative", filename="prof.txt"):
+        """
+            Description: Dump the total statistics from this current profile to a
+            file. Cleared by calls to reset. If running profile over multiple trials,
+            this dumps cumulative runtime across all trials: use dumpAverageStats()
+            instead.
+        """
         with open(filename, "w") as f:
             sts = pstats.Stats(self.prf, stream=f)
             sts.sort_stats(sortby)
@@ -108,7 +149,14 @@ class Profiler:
             if len(obj[stat]) > 4 and obj[stat][4] is not {}:
                 self.calcAvgStatsHelper(obj[stat][4], num_of_runs)
 
-    def dumpAverageStats(self, sortby, filename, num_of_runs):
+    def dumpAverageStats(
+        self, sortby="cumulative", filename="avg_prof.txt", num_of_runs=1
+    ):
+        """
+            Description: Dump the average runtime statistics from this current profile to a
+            file. Cleared by calls to reset. All runtimes and statistics for every function call
+            is averaged over num_of_runs.
+        """
         with open(filename, "w") as f:
             f.write("\n\n Averaged over {} trials \n\n".format(num_of_runs))
             sts = pstats.Stats(self.prf, stream=f)
