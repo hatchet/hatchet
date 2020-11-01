93c8cb07ae5125b9b309a32d4330bf0ef464d192
Abhinav Bhatele <bhatele@cs.umd.edu>
2020-11-01 11:19:29 -0500

    License text update

--- hatchet/tests/timemory_func.py
+++ hatchet/tests/timemory_func.py
@@ -1,5 +1,5 @@
 # Copyright 2020 The Regents of the University of California, through Lawrence
-# Berkeley National Laboratory and other Hatchet Project Developers. See the
+# Berkeley National Laboratory, and other Hatchet Project Developers. See the
 # top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
