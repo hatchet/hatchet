fd444525db8f2d4171ffd4f6f7b3a5163a6c41b9
Stephanie Brink <brink2@llnl.gov>
2021-01-05 14:39:58 -0800

    switch caliper_db_reader to take a CaliperReader object

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "hatchet.tests",
         "hatchet.cython_modules.libs",
     ],
-    install_requires=["pydot", "PyYAML", "matplotlib", "numpy", "pandas"],
+    install_requires=["pydot", "PyYAML", "matplotlib", "numpy", "pandas", "caliper-reader"],
     ext_modules=[
         Extension(
             "hatchet.cython_modules.libs.reader_modules",
