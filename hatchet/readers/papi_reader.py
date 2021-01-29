41864ef8ee5e19134cd6acb58f29611eb1893d4f
Frank Winkler <frank.winkler@tu-dresden.de>
2021-01-29 14:21:10 +0100

    Test example for system tree and instrumented regions.

--- hatchet/readers/papi_reader.py
+++ hatchet/readers/papi_reader.py
@@ -24,15 +24,139 @@ class PAPIReader:
         list_roots = []
         node_dicts = []
 
-#######
-        node_name = "do_work"
-        frame = Frame({"type": "region", "name": node_name})
+####### call tree
+        # node_name = "do_work"
+        # frame = Frame({"type": "region", "name": node_name})
+        # graph_root = Node(frame, None)
+        # node_dict = dict(
+        #   {"node": graph_root, "name": node_name,
+        #   'rank': 0,
+        #      'thread': 0,
+        #      'region_count': 1,
+        #      "cycles": 2025033750,
+        #      "real_time_nsec": 1015090454,
+        #      "perf::TASK-CLOCK": 14980591,
+        #      "PAPI_TOT_INS": 31869696,
+        #      "PAPI_TOT_CYC": 29027697,
+        #      "PAPI_FP_INS": 0,
+        #      "PAPI_FP_OPS": 54}
+        # )
+        # node_dicts.append(node_dict)
+
+        # node_name = "do_work(read)"
+        # frame1 = Frame({"type": "region", "name": node_name})
+        # child_root = Node(frame1, graph_root)
+        # node_dict = dict(
+        #   {"node": child_root, "name": node_name,
+        #   'rank': 0,
+        #      'thread': 0,
+        #      'region_count': 1,
+        #      "cycles": 2025033750,
+        #      "real_time_nsec": 1015090454,
+        #      "perf::TASK-CLOCK": 14980591,
+        #      "PAPI_TOT_INS": 31869696,
+        #      "PAPI_TOT_CYC": 29027697,
+        #      "PAPI_FP_INS": 0,
+        #      "PAPI_FP_OPS": 54}
+        # )
+        # node_dicts.append(node_dict)
+        # graph_root.add_child(child_root)
+        # list_roots.append(graph_root)
+
+
+        # node_name = "do_work"
+        # frame2 = Frame({"type": "region", "name": node_name })
+        # graph_root2 = Node(frame2, None)
+        # node_dict = dict(
+        #   {"node": graph_root2, "name": node_name,
+        #   'rank': 0,
+        #      'thread': 1,
+        #      'region_count': 1,
+        #      "cycles": 2025736622,
+        #      "real_time_nsec": 1015445611,
+        #      "perf::TASK-CLOCK": 15279270,
+        #      "PAPI_TOT_INS": 31873731,
+        #      "PAPI_TOT_CYC": 28806889,
+        #      "PAPI_FP_INS": 0,
+        #      "PAPI_FP_OPS": 98}
+        # )
+        # node_dicts.append(node_dict)
+        # list_roots.append(graph_root2)
+
+        # node_name = "do_work"
+        # frame3 = Frame({"type": "region", "name": node_name })
+        # graph_root3 = Node(frame3, None)
+        # node_dict = dict(
+        #   {"node": graph_root3, "name": node_name,
+        #   'rank': 1,
+        #      'thread': 0,
+        #      'region_count': 1,
+        #      "cycles": 2030360894,
+        #      "real_time_nsec": 1017576862,
+        #      "perf::TASK-CLOCK": 17452816,
+        #      "PAPI_TOT_INS": 31902057,
+        #      "PAPI_TOT_CYC": 30709312,
+        #      "PAPI_FP_INS": 0,
+        #      "PAPI_FP_OPS": 149}
+        # )
+        # node_dicts.append(node_dict)
+        # list_roots.append(graph_root3)
+
+        # node_name = "do_work"
+        # frame4 = Frame({"type": "region", "name": node_name })
+        # graph_root4 = Node(frame4, None)
+        # node_dict = dict(
+        #   {"node": graph_root4, "name": node_name,
+        #   'rank': 1,
+        #      'thread': 1,
+        #      'region_count': 1,
+        #      "cycles": 2030360933,
+        #      "real_time_nsec": 1017570681,
+        #      "perf::TASK-CLOCK": 17515841,
+        #      "PAPI_TOT_INS": 32087423,
+        #      "PAPI_TOT_CYC": 30683338,
+        #      "PAPI_FP_INS": 0,
+        #      "PAPI_FP_OPS": 25}
+        # )
+        # node_dicts.append(node_dict)
+        # list_roots.append(graph_root4)
+#######        
+
+####### system tree including instrumented regions
+        process_metrics = {
+          "region_count": '',
+          "cycles": '',
+          "real_time_nsec": '',
+          "perf::TASK-CLOCK": None,
+          "PAPI_TOT_INS": '',
+          "PAPI_TOT_CYC": '',
+          "PAPI_FP_INS": '',
+          "PAPI_FP_OPS": ''
+        }
+
+        node_name = "MPI Rank 0"
+        frame = Frame({"type": "rank", "name": node_name})
         graph_root = Node(frame, None)
         node_dict = dict(
-          {"node": graph_root, "name": node_name,
-          'rank': 0,
-             'thread': 0,
-             'region_count': 1,
+          {"node": graph_root, "name": node_name, **process_metrics}
+        )
+        node_dicts.append(node_dict)
+
+        node_name = "CPU thread 0"
+        frame = Frame({"type": "thread", "name": node_name})
+        graph_root1 = Node(frame, graph_root)
+        node_dict = dict(
+          {"node": graph_root1, "name": node_name, **process_metrics}
+        )
+        node_dicts.append(node_dict)
+        graph_root.add_child(graph_root1)
+
+        node_name = "do_work"
+        frame = Frame({"type": "region", "name": node_name})
+        graph_root2 = Node(frame, graph_root1)
+        node_dict = dict(
+          {"node": graph_root2, "name": node_name,
+             "region_count": 1,
              "cycles": 2025033750,
              "real_time_nsec": 1015090454,
              "perf::TASK-CLOCK": 14980591,
@@ -42,65 +166,38 @@ class PAPIReader:
              "PAPI_FP_OPS": 54}
         )
         node_dicts.append(node_dict)
-        list_roots.append(graph_root)
+        graph_root1.add_child(graph_root2)
 
-        node_name = "do_work"
-        frame = Frame({"type": "region", "name": node_name })
-        graph_root = Node(frame, None)
+        node_name = "CPU thread 1"
+        frame = Frame({"type": "thread", "name": node_name})
+        graph_root3 = Node(frame, graph_root)
         node_dict = dict(
-          {"node": graph_root, "name": node_name,
-          'rank': 0,
-             'thread': 1,
-             'region_count': 1,
-             "cycles": 2025736622,
-             "real_time_nsec": 1015445611,
-             "perf::TASK-CLOCK": 15279270,
-             "PAPI_TOT_INS": 31873731,
-             "PAPI_TOT_CYC": 28806889,
-             "PAPI_FP_INS": 0,
-             "PAPI_FP_OPS": 98}
+          {"node": graph_root3, "name": node_name, **process_metrics}
         )
         node_dicts.append(node_dict)
-        list_roots.append(graph_root)
+        graph_root.add_child(graph_root3)
 
         node_name = "do_work"
-        frame = Frame({"type": "region", "name": node_name })
-        graph_root = Node(frame, None)
+        frame = Frame({"type": "region", "name": node_name})
+        graph_root4 = Node(frame, graph_root3)
         node_dict = dict(
-          {"node": graph_root, "name": node_name,
-          'rank': 1,
-             'thread': 0,
-             'region_count': 1,
-             "cycles": 2030360894,
-             "real_time_nsec": 1017576862,
-             "perf::TASK-CLOCK": 17452816,
-             "PAPI_TOT_INS": 31902057,
-             "PAPI_TOT_CYC": 30709312,
+          {"node": graph_root4, "name": node_name,
+             "region_count": 1,
+             "cycles": 2025033750,
+             "real_time_nsec": 1015090454,
+             "perf::TASK-CLOCK": 14980591,
+             "PAPI_TOT_INS": 31869696,
+             "PAPI_TOT_CYC": 29027697,
              "PAPI_FP_INS": 0,
-             "PAPI_FP_OPS": 149}
+             "PAPI_FP_OPS": 54}
         )
         node_dicts.append(node_dict)
+        graph_root3.add_child(graph_root4)
+
         list_roots.append(graph_root)
 
-        node_name = "do_work"
-        frame = Frame({"type": "region", "name": node_name })
-        graph_root = Node(frame, None)
-        node_dict = dict(
-          {"node": graph_root, "name": node_name,
-          'rank': 1,
-             'thread': 1,
-             'region_count': 1,
-             "cycles": 2030360933,
-             "real_time_nsec": 1017570681,
-             "perf::TASK-CLOCK": 17515841,
-             "PAPI_TOT_INS": 32087423,
-             "PAPI_TOT_CYC": 30683338,
-             "PAPI_FP_INS": 0,
-             "PAPI_FP_OPS": 25}
-        )
-        node_dicts.append(node_dict)
-        list_roots.append(graph_root)
-#######        
+#######
+
 
         #print("list_roots ", list_roots)
         #print("node_dicts ", node_dicts)
@@ -108,7 +205,7 @@ class PAPIReader:
         graph = Graph(list_roots)
         graph.enumerate_traverse()
 
-        exc_metrics = [
+        inc_metrics = [
           'region_count',
           'cycles',
           'real_time_nsec',
@@ -119,12 +216,7 @@ class PAPIReader:
           'PAPI_FP_OPS'
         ]
 
-        inc_metrics = []
-        # for key in self.graph_dict[i]["metrics"].keys():
-        #     if "(inc)" in key:
-        #         inc_metrics.append(key)
-        #     else:
-        #         exc_metrics.append(key)
+        exc_metrics = []
 
         dataframe = pd.DataFrame(data=node_dicts)
         dataframe.columns = dataframe.columns.str.strip()
@@ -134,22 +226,11 @@ class PAPIReader:
         #print(dataframe.columns.tolist())
         # print("\n\n")
 
-        # print(dataframe.columns)
-        # dataframe.set_index(["node", "rank", "thread"], inplace=True)
-        # print(dataframe.columns)
-
-        #dataframe.reset_index(drop=True, inplace=True)
-        #dataframe.set_index(["node", "rank", "thread"], inplace=True)
-        #dataframe.set_index(["rank"], inplace=True)
-        
-        # print(dataframe.columns.tolist())
-        # print("\n\n")
-        #dataframe.sort_index(inplace=True)
-
         #return hatchet.graphframe.GraphFrame(graph, dataframe, exc_metrics, inc_metrics)
 
         dataframe.set_index(["node"], inplace=True)
         #dataframe.set_index(["node", "rank", "thread"], inplace=True)
         print(dataframe)
         gf = hatchet.graphframe.GraphFrame(graph, dataframe, exc_metrics, inc_metrics)
-        print(gf.tree(metric_column="perf::TASK-CLOCK", context_column="region_count", rank=1, thread=1))
+        #print(gf.tree(metric_column="perf::TASK-CLOCK", context_column="region_count", rank=1, thread=1))
+        print(gf.tree(metric_column="perf::TASK-CLOCK", context_column="region_count"))
