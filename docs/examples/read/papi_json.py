c1f4545ad077f314468dfabcbdc911f80c8f74ff
Frank Winkler <frank.winkler@tu-dresden.de>
2021-04-19 11:16:15 +0200

    Added PAPI performance report example.

--- docs/examples/read/papi_json.py
+++ docs/examples/read/papi_json.py
@@ -6,23 +6,11 @@ import hatchet as ht
 
 if __name__ == "__main__":
     # Define a literal GraphFrame
-    #gf = ht.GraphFrame.from_papi("/home/fwinkler/applications/papi_hybrid/papi_hl_output")
-    gf = ht.GraphFrame.from_papi("/home/fwinkler/rank_000000.json", ['kokkosp_parallel_for', 'kokkosp_deep_copy', 'kokkosp_parallel_scan', 'kokkosp_parallel_reduce'])
+    gf = ht.GraphFrame.from_papi("../../../hatchet/tests/data/papi_hl_output")
 
     # Printout the DataFrame component of the GraphFrame.
-    #print(gf.dataframe.to_string())
     print(gf.dataframe)
 
     # Printout the graph component of the GraphFrame.
-    # for rank in range(0, 2):
-    #   for thread in range(0, 2):
-    #     print("\nrank={}, thread={}".format(rank, thread))
-    #     print(gf.tree(metric_column="real_time_nsec", rank=rank, thread=thread))
-
-    print(gf.tree(metric_column="real_time_nsec"))
-    # print(gf.tree(metric_column="PAPI_TOT_INS"))
-    # print(gf.tree(metric_column="PAPI_TOT_CYC"))
-    # print(gf.tree(metric_column="PAPI_FP_INS"))
-    # print(gf.tree(metric_column="rocm:::TCC_HIT_sum:device=0"))
-    # print(gf.tree(metric_column="rocm:::TCC_MISS_sum:device=0"))
-    # print(gf.tree(metric_column="rocm:::L2CacheHit:device=0"))
+    # Use "PAPI_FP_OPS" as the metric column to be displayed
+    print(gf.tree(metric_column="PAPI_FP_OPS"))
