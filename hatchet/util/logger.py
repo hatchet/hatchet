36e56014ee489fa434c4c9baed4ac503c7122422
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2021-04-22 11:39:55 -0700

    Added logging param to from_* functions, modified data design, disabled logging of nested nested

--- hatchet/util/logger.py
+++ hatchet/util/logger.py
@@ -4,59 +4,78 @@ import json
 from datetime import datetime
 import hatchet
 
-class Log(object):    
-    def __init__(self, filename="hatchet_log.json", active=True):
+
+class Log(object):
+    def __init__(self, filename="hatchet_log.json", active=False):
         self._log_file = filename
         self._active = active
+        self._nested = False  # ensures we only log user called commands
 
     def set_output_file(self, filename=""):
         self._log_file = filename
 
     def set_active(self):
         self._active = True
-    
+
     def set_inactive(self):
         self._active = False
 
     def append_to_file(self, log):
+        """Manages the opening and writing of log information to a file."""
         logs = []
         if os.path.exists(self._log_file):
-            with open(self._log_file, 'r') as f:
+            with open(self._log_file, "r") as f:
                 logs = json.loads(f.read())
-        
+
         logs.append(log)
-        with open(self._log_file, 'w+') as f:
-                f.write(json.dumps(logs))    
+        with open(self._log_file, "w+") as f:
+            f.write(json.dumps(logs))
 
     def loggable(self, function):
-        '''A decrator which logs calls to hatchet functions'''
+        """A decrator which logs calls to hatchet functions"""
+
         def inner(*args, **kwargs):
-            if self._active:
-                
-                log_dict = {}    
+            # turn on logger
+            if "logging" in kwargs and kwargs["logging"] is True:
+                self._active = True
+            elif "logging" in kwargs and kwargs["logging"] is False:
+                self._active = False
+
+            # if logger is on
+            if self._active and self._nested is False:
+                log_dict = {}
                 arg_list = []
-                graphframe_metadata = {}
+
+                log_dict["function"] = function.__name__
 
                 for i, arg in enumerate(args):
-                    if(inspect.isfunction(arg)):
+                    if inspect.isfunction(arg):
+                        # log functions passed (eg. lambda passed to filter) as source code
                         arg_source = inspect.getsource(arg)
                         arg_list.append(arg_source)
-                    elif isinstance(arg, hatchet.GraphFrame):
-                        graphframe_metadata['rows'] = arg.dataframe.shape[0]
-                        graphframe_metadata['nodes'] = len(arg.graph)
-                        # arg_list.append(arg)
-                    else:
-                        arg_list.append(arg)
-                
-                log_dict['graphframe_metadata'] = graphframe_metadata
-                log_dict['args'] = tuple(arg_list)
-                log_dict['start'] = datetime.now().isoformat()
-                
-                holder =  function(*args, **kwargs)
 
-                log_dict['end'] = datetime.now().isoformat()
-                log_dict['function'] = function.__name__
-                log_dict['kwargs'] = kwargs
+                    elif isinstance(arg, hatchet.GraphFrame):
+                        # log a graphframe as a dictionary of metadata
+                        graphframe_metadata = {}
+
+                        graphframe_metadata["id"] = id(arg)
+                        graphframe_metadata["rows"] = arg.dataframe.shape[0]
+                        graphframe_metadata["nodes"] = len(arg.graph)
+
+                        arg_list.append(graphframe_metadata)
+                    else:
+                        # log everything else
+                        arg_list.append(arg)
+
+                log_dict["args"] = tuple(arg_list)
+                log_dict["start"] = datetime.now().isoformat()
+
+                self._nested = True
+                holder = function(*args, **kwargs)
+                self._nested = False
+
+                log_dict["end"] = datetime.now().isoformat()
+                log_dict["kwargs"] = kwargs
 
                 self.append_to_file(log_dict)
 
@@ -65,5 +84,6 @@ class Log(object):
                 return function(*args, **kwargs)
 
         return inner
+
 
 Logger = Log()
