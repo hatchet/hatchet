dc4204230c702ab611e1efe8e4f63720e5933966
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2021-05-07 13:33:27 -0700

    Formatting fixes

--- hatchet/util/rcmanager.py
+++ hatchet/util/rcmanager.py
@@ -5,40 +5,41 @@ except ImportError:
 import os.path as path
 import yaml
 
+
 class RcManager(MutableMapping, dict):
-    '''
-        A runtime configurations class; modeled after the RcParams class in matplotlib. 
-        The benifit of this class over a dictonary is validation of set item and formatting
-        of output.
-    '''
+    """
+    A runtime configurations class; modeled after the RcParams class in matplotlib.
+    The benifit of this class over a dictonary is validation of set item and formatting
+    of output.
+    """
 
     def __init__(self, *args, **kwargs):
         self.update(*args, **kwargs)
 
     def __setitem__(self, key, val):
-        '''
-            We can put validation of configs here.
-        '''
+        """
+        We can put validation of configs here.
+        """
         return dict.__setitem__(self, key, val)
-    
+
     def __getitem__(self, key):
         return dict.__getitem__(self, key)
-    
+
     def __iter__(self):
         for kv in sorted(dict.__iter__(self)):
             yield kv
 
     def __str__(self):
-        return '\n'.join(map('{0[0]}: {0[1]}'.format, sorted(self.items())))
+        return "\n".join(map("{0[0]}: {0[1]}".format, sorted(self.items())))
 
 
 def _resolve_conf_file():
-    '''
-        Determines which configuration file to load.
-        Uses the precendence order:
-            1. $HOME/.config/hatchet/hatchetrc.yaml
-            2. $HATCHET_BASE_DIR/hatchetrc.yaml
-    '''
+    """
+    Determines which configuration file to load.
+    Uses the precendence order:
+        1. $HOME/.config/hatchet/hatchetrc.yaml
+        2. $HATCHET_BASE_DIR/hatchetrc.yaml
+    """
     home = path.expanduser("~")
     conf_dir = path.join(home, ".config", "hatchet", "hatchetrc.yaml")
     if path.exists(conf_dir):
@@ -48,15 +49,16 @@ def _resolve_conf_file():
         rel_path = path.join(my_path, "..", "..", "hatchetrc.yaml")
         return rel_path
 
+
 def _read_config_from_file(filepath):
     configs = {}
-    with open(filepath, 'r') as f:
+    with open(filepath, "r") as f:
         configs = yaml.load(f, yaml.FullLoader)
     return configs
+
 
 filepath = _resolve_conf_file()
 configs = _read_config_from_file(filepath)
 
 # Global instance of configurations
 RcParams = RcManager(configs)
-
