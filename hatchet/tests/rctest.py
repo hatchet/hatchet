cc66070d711d42037444fe8180e19c8a1348cda8
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2021-05-07 15:34:52 -0700

    added tests

---
+++ hatchet/tests/rctest.py
@@ -0,0 +1,35 @@
+from hatchet.util.rcmanager import RcManager, _resolve_conf_file, _read_config_from_file
+
+
+def test_creation():
+    file = _resolve_conf_file()
+    config = _read_config_from_file(file)
+    RC = RcManager(config)
+
+    assert RC is not None
+
+
+def test_global_binding():
+    import hatchet as ht
+
+    assert hasattr(ht, "RcParams")
+
+
+def test_change_value():
+    import hatchet as ht
+
+    assert "logging" in ht.RcParams
+
+    ht.RcParams["logging"] = True
+    assert ht.RcParams["logging"] is True
+
+    from hatchet.util.rcmanager import RcParams
+
+    assert RcParams["logging"] is True
+
+    ht.RcParams["logging"] = False
+    assert ht.RcParams["logging"] is False
+
+    from hatchet.util.rcmanager import RcParams
+
+    assert RcParams["logging"] is False
