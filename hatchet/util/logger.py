ab722927b324fc763f7fb995f9705d7ee3dbb926
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2021-06-08 11:55:11 -0700

    Incorporated global configuration interface into logger configs with dummy interface. Waiting on merge of global interface.

--- hatchet/util/logger.py
+++ hatchet/util/logger.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+import errno
 import getpass
 import json
 from datetime import datetime
@@ -7,8 +8,9 @@ import hatchet
 
 RcParams = {"logging": True, "log_directory": "~/.hatchet/logs/"}
 
+
 class Log(object):
-    def __init__(self, filename='hatchet.log', active=None):
+    def __init__(self, filename="hatchet.log", active=None):
         self._log_file = filename
         if active is not None:
             self._active = active
@@ -17,8 +19,7 @@ class Log(object):
 
         # ensures we only log api calls made explicitly by
         # a user
-        self._nested = False  
-
+        self._nested = False
 
     def set_output_file(self, filename=""):
         self._log_file = filename
@@ -29,79 +30,78 @@ class Log(object):
     def set_inactive(self):
         self._active = False
 
-    def read(self):
-        logs = []
-        with open(self._log_file, "r") as f:
-            for line in f.readlines():
-                logs.append(json.loads(line))
-            print(logs)
-
     def append_to_file(self, log):
         """Manages the opening and writing of log information to a file."""
-        with open(self._log_file, "a") as f:
-            f.write(json.dumps(log)+'\n')
+        logpath = os.path.expanduser(RcParams["log_directory"])
+        if not os.path.exists(logpath):
+            try:
+                os.makedirs(logpath)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
+        logpath = os.path.join(logpath, self._log_file)
+        with open(logpath, "a") as f:
+            f.write(json.dumps(log) + "\n")
 
     def loggable(self, function):
         """A decrator which logs calls to hatchet functions"""
 
         def inner(*args, **kwargs):
-            # turn on logger
-            if "logging" in kwargs and kwargs["logging"] is True:
-                self._active = True
-            elif "logging" in kwargs and kwargs["logging"] is False:
-                self._active = False
+            try:
+                # if logger is on and user called function
+                if self._active and not self._nested:
+                    log_dict = {}
+                    arg_list = []
 
-            # if logger is on and user called function
-            if self._active and self._nested is False:
-                log_dict = {}
-                arg_list = []
+                    # Get a  user id
+                    try:
+                        log_dict["user_id"] = os.getuid()
+                    except AttributeError:
+                        # for windows machines
+                        log_dict["user_id"] = getpass.getuser()
 
-                # Get a  user id
-                try:
-                    log_dict["user_id"] = os.getuid()
-                except:
-                    log_dict["user_id"] = getpass.getuser()
+                    log_dict["function"] = function.__name__
 
-                log_dict["function"] = function.__name__
-                
-                # for call in inspect.stack(context=1):
-                    # if 'usr' in call.filename and 'tce' not in call.filename:
-                        # print(call.filename)
+                    for i, arg in enumerate(args):
+                        if inspect.isfunction(arg):
+                            # log functions passed (eg. lambda passed to filter) as source code
+                            arg_source = inspect.getsource(arg)
+                            arg_list.append(arg_source)
 
-                for i, arg in enumerate(args):
-                    if inspect.isfunction(arg):
-                        # log functions passed (eg. lambda passed to filter) as source code
-                        arg_source = inspect.getsource(arg)
-                        arg_list.append(arg_source)
+                        elif isinstance(arg, hatchet.GraphFrame):
+                            # log a graphframe as a dictionary of metadata
+                            graphframe_metadata = {}
 
-                    elif isinstance(arg, hatchet.GraphFrame):
-                        # log a graphframe as a dictionary of metadata
-                        graphframe_metadata = {}
+                            graphframe_metadata["object"] = arg.__class__.__name__
+                            graphframe_metadata["id"] = id(arg)
+                            graphframe_metadata["rows"] = arg.dataframe.shape[0]
+                            graphframe_metadata["nodes"] = len(arg.graph)
 
-                        graphframe_metadata["object"] = arg.__class__.__name__
-                        graphframe_metadata["id"] = id(arg)
-                        graphframe_metadata["rows"] = arg.dataframe.shape[0]
-                        graphframe_metadata["nodes"] = len(arg.graph)
+                            arg_list.append(graphframe_metadata)
+                        else:
+                            # log everything else
+                            arg_list.append(arg)
 
-                        arg_list.append(graphframe_metadata)
-                    else:
-                        # log everything else
-                        arg_list.append(arg)
+                    log_dict["args"] = tuple(arg_list)
+                    log_dict["start"] = datetime.now().isoformat()
 
-                log_dict["args"] = tuple(arg_list)
-                log_dict["start"] = datetime.now().isoformat()
+                    self._nested = True
+                    holder = function(*args, **kwargs)
+                    self._nested = False
 
-                self._nested = True
-                holder = function(*args, **kwargs)
-                self._nested = False
+                    log_dict["end"] = datetime.now().isoformat()
+                    log_dict["kwargs"] = kwargs
 
-                log_dict["end"] = datetime.now().isoformat()
-                log_dict["kwargs"] = kwargs
+                    self.append_to_file(log_dict)
 
-                self.append_to_file(log_dict)
+                    return holder
+                else:
+                    return function(*args, **kwargs)
 
-                return holder
-            else:
+            # If there is a file io error when logging
+            # we run function as normal and abandon log
+            except IOError:
                 return function(*args, **kwargs)
 
         return inner
