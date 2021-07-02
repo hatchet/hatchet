4853a732cec2109d86440642b831ed26040b0558
Stephanie Brink <brink2@llnl.gov>
2021-07-02 12:57:37 -0700

    update example read filenames, add tests
    - filenames cannot be named the same as python modules, environment tries to
      import the example code, not the actual module

--- docs/examples/read/caliper_caliperreader.py
+++ docs/examples/read/caliper_caliperreader.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
+import caliperreader as cr
+
 import hatchet as ht
 
 
 if __name__ == "__main__":
     # Path to caliper cali file.
-    cali_file = (
-        "../../../hatchet/tests/data/caliper-lulesh-cali/lulesh-annotation-profile.cali"
-    )
+    cali_file = "../../../hatchet/tests/data/caliper-example-cali/example-profile.cali"
 
-    gf = ht.GraphFrame.from_caliperreader(cali_file)
+    r = cr.CaliperReader()
+    r.read(cali_file)
+
+    gf = ht.GraphFrame.from_caliperreader(r)
 
     # Printout the DataFrame component of the GraphFrame.
     print(gf.dataframe)
--- docs/examples/read/caliper_caliperreader_cali.py
+++ docs/examples/read/caliper_caliperreader_cali.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import caliperreader as cr
-
 import hatchet as ht
 
 
@@ -9,13 +7,10 @@ if __name__ == "__main__":
     # Path to caliper cali file.
     cali_file = "../../../hatchet/tests/data/caliper-example-cali/example-profile.cali"
 
-    r = cr.CaliperReader()
-    r.read(cali_file)
-
-    gf = ht.GraphFrame.from_caliperreader(r)
+    gf = ht.GraphFrame.from_caliperreader(cali_file)
 
     # Printout the DataFrame component of the GraphFrame.
-    print(gf.dataframe.columns)
+    print(gf.dataframe)
 
     # Printout the graph component of the GraphFrame with the specified metric.
     print(gf.tree(metric_column="avg#inclusive#sum#time.duration"))
