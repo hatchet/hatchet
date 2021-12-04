51925573bedd7e70d9ac909c000e4e2f904f0abf
Stephanie Brink <brink2@llnl.gov>
2021-12-03 16:02:10 -0800

    fixes and cleanup

--- hatchet/readers/caliper_native_reader.py
+++ hatchet/readers/caliper_native_reader.py
@@ -29,14 +29,17 @@ class CaliperNativeReader:
         self.filename_or_caliperreader = filename_or_caliperreader
         self.filename_ext = ""
 
+        self.df_nodes = {}
         self.metric_columns = []
         self.record_cols_mdata = []
         self.node_dicts = []
-
-        self.df_nodes = {}
         self.callpath_to_node = {}
+        self.node_to_nid = {}
+        self.idx_to_node = {}
+        self.callpath_to_metrics = {}
         self.callpath_to_idx = {}
         self.global_nid = 0
+        self.global_nid2 = 0
 
         self.timer = Timer()
 
@@ -45,20 +48,27 @@ class CaliperNativeReader:
 
     def read_metrics(self, ctx="path"):
         all_metrics = []
+        records = self.filename_or_caliperreader.records
 
-        for record in self.filename_or_caliperreader.records:
+        for record in records:
             node_dict = {}
             if ctx in record:
                 if isinstance(record[ctx], list):
-                    node_callpath = tuple(record[ctx])
+                    if "cupti.activity.kind" in record:
+                        if record["cupti.activity.kind"] == "kernel":
+                            node_label = record["cupti.kernel.name"]
+                            node_callpath = tuple(record[ctx] + [node_label])
+                        elif record["cupti.activity.kind"] == "memcpy":
+                            node_label = record["cupti.activity.kind"]
+                            node_callpath = tuple(record[ctx] + [node_label])
+                    else:
+                        node_label = record[ctx][-1]
+                        node_callpath = tuple(record[ctx])
                 else:
+                    node_label = record[ctx][-1]
                     node_callpath = tuple([record[ctx]])
-
                 nid = self.callpath_to_idx.get(node_callpath)
-                if nid is not None:
-                    node_dict["nid"] = int(nid)
-                else:
-                    pass
+                node_dict["nid"] = nid
 
                 for item in record.keys():
                     if item != ctx:
@@ -87,6 +97,7 @@ class CaliperNativeReader:
                         else:
                             node_dict[item] = record[item]
 
+                self.global_nid2 = self.global_nid2 + 1
                 all_metrics.append(node_dict)
 
         for col in self.record_cols_mdata:
@@ -97,7 +108,7 @@ class CaliperNativeReader:
         return df_metrics
 
     def create_graph(self, ctx="path"):
-        def _create_parent(hnode, parent_callpath):
+        def _create_parent(child_node, parent_callpath):
             """We may encounter a parent node in the callpath before we see it
             as a child node. In this case, we need to create a hatchet node for
             the parent.
@@ -113,97 +124,109 @@ class CaliperNativeReader:
             # return if arrives at the parent
             # else create a parent and add parent/child
             if parent_node:
-                parent_node.add_child(hnode)
-                hnode.add_parent(parent_node)
+                parent_node.add_child(child_node)
+                child_node.add_parent(parent_node)
                 return
             else:
                 grandparent_callpath = parent_callpath[:-1]
                 parent_name = parent_callpath[-1]
 
                 parent_node = Node(
-                    Frame({"type": "function", "name": parent_name}),
-                    None,
-                    hnid=self.global_nid,
+                    Frame({"type": "function", "name": parent_name}), None
                 )
 
                 self.callpath_to_node[parent_callpath] = parent_node
                 self.callpath_to_idx[parent_callpath] = self.global_nid
+
                 node_dict = dict(
                     {"name": parent_name, "node": parent_node, "nid": self.global_nid},
                 )
-                self.node_dicts.append(node_dict)
+                self.idx_to_node[self.global_nid] = node_dict
                 self.global_nid += 1
 
-                parent_node.add_child(hnode)
-                hnode.add_parent(parent_node)
+                parent_node.add_child(child_node)
+                child_node.add_parent(parent_node)
                 _create_parent(parent_node, grandparent_callpath)
 
         list_roots = []
         parent_hnode = None
 
-        # find records containing the path hierarchy
         for record in self.filename_or_caliperreader.records:
             node_label = ""
-
             if ctx in record:
                 # if it's a list, then it's a callpath
                 if isinstance(record[ctx], list):
-                    node_label = record[ctx][-1]
-                    node_callpath = tuple(record[ctx])
-                    parent_callpath = node_callpath[:-1]
-                    node_type = "function"
+                    if "cupti.activity.kind" in record:
+                        if record["cupti.activity.kind"] == "kernel":
+                            node_label = record["cupti.kernel.name"]
+                            node_callpath = tuple(record[ctx] + [node_label])
+                            parent_callpath = node_callpath[:-1]
+                            node_type = "kernel"
+                        elif record["cupti.activity.kind"] == "memcpy":
+                            node_label = record["cupti.activity.kind"]
+                            node_callpath = tuple(record[ctx] + [node_label])
+                            parent_callpath = node_callpath[:-1]
+                            node_type = "memcpy"
+                        else:
+                            Exception("Haven't seen this activity kind yet")
+                    else:
+                        node_label = record[ctx][-1]
+                        node_callpath = tuple(record[ctx])
+                        parent_callpath = node_callpath[:-1]
+                        node_type = "function"
 
                     hnode = self.callpath_to_node.get(node_callpath)
-                    if not hnode:
-                        # create the node since it does not exist
-                        frame = Frame({"type": node_type, "name": node_label})
-                        hnode = Node(frame, None, hnid=self.global_nid)
 
-                        # store callpaths to identify the node
-                        self.callpath_to_idx[node_callpath] = self.global_nid
+                    if not hnode:
+                        frame = Frame({"type": node_type, "name": node_label})
+                        hnode = Node(frame, None)
                         self.callpath_to_node[node_callpath] = hnode
 
                         # get parent from node callpath
                         parent_hnode = self.callpath_to_node.get(parent_callpath)
 
                         if not parent_hnode:
+                            # create parent since it doesn't exist
                             _create_parent(hnode, parent_callpath)
                         else:
                             parent_hnode.add_child(hnode)
                             hnode.add_parent(parent_hnode)
 
-                    node_dict = dict(
-                        {"name": node_label, "node": hnode, "nid": self.global_nid}
-                    )
-                    self.node_dicts.append(node_dict)
-                    self.global_nid += 1
+                        self.callpath_to_idx[node_callpath] = self.global_nid
+                        node_dict = dict(
+                            {"name": node_label, "node": hnode, "nid": self.global_nid},
+                        )
+                        self.idx_to_node[self.global_nid] = node_dict
+                        self.global_nid += 1
+
                 # if it's a string, then it's a root
                 else:
                     root_label = record[ctx]
                     root_callpath = tuple([root_label])
 
                     if root_callpath not in self.callpath_to_node:
-                        # create the root since it does not exist
+                        # create the root since it doesn't exist
                         frame = Frame({"type": "function", "name": root_label})
-                        graph_root = Node(frame, None, hnid=self.global_nid)
+                        graph_root = Node(frame, None)
 
                         # store callpaths to identify the root
                         self.callpath_to_node[root_callpath] = graph_root
                         self.callpath_to_idx[root_callpath] = self.global_nid
                         list_roots.append(graph_root)
-                    else:
-                        # do not create a new root node since it was already created
-                        graph_root = self.callpath_to_node.get(root_callpath)
 
-                    node_dict = dict(
-                        {
-                            "name": root_label,
-                            "node": graph_root,
-                            "nid": self.global_nid,
-                        }
-                    )
-                    self.node_dicts.append(node_dict)
-                    self.global_nid += 1
+                        node_dict = dict(
+                            {
+                                "name": root_label,
+                                "node": graph_root,
+                                "nid": self.global_nid,
+                            }
+                        )
+
+                        self.idx_to_node[self.global_nid] = node_dict
+                        self.global_nid += 1
+                    else:
+                        # don't create a new node since it was already created
+                        graph_root = self.callpath_to_node.get(root_callpath)
 
         return list_roots
 
@@ -219,8 +242,7 @@ class CaliperNativeReader:
 
         with self.timer.phase("graph construction"):
             list_roots = self.create_graph()
-
-        self.df_nodes = pd.DataFrame(data=self.node_dicts)
+        self.df_nodes = pd.DataFrame(data=list(self.idx_to_node.values()))
 
         # create a graph object once all the nodes have been added
         graph = Graph(list_roots)
@@ -229,9 +251,11 @@ class CaliperNativeReader:
         with self.timer.phase("read metrics"):
             df_fixed_data = self.read_metrics()
 
+        metrics = pd.DataFrame.from_dict(data=df_fixed_data)
+
         # add missing intermediate nodes to the df_fixed_data dataframe
         if "mpi.rank" in df_fixed_data.columns:
-            num_ranks = df_fixed_data["mpi.rank"].max() + 1
+            num_ranks = metrics["mpi.rank"].max() + 1
             rank_list = range(0, num_ranks)
 
         # create a standard dict to be used for filling all missing rows
@@ -244,9 +268,9 @@ class CaliperNativeReader:
 
         # create a list of dicts, one dict for each missing row
         missing_nodes = []
-        for _, row in self.df_nodes.iterrows():
+        for iteridx, row in self.df_nodes.iterrows():
             # check if df_nodes row exists in df_fixed_data
-            metric_rows = df_fixed_data.loc[df_fixed_data["nid"] == row["nid"]]
+            metric_rows = df_fixed_data.loc[metrics["nid"] == row["nid"]]
             if "mpi.rank" not in self.metric_columns:
                 if metric_rows.empty:
                     # add a single row
