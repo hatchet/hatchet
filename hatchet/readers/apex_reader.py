a61769358662a87fec1b9a4bde1a80c55272b68b
Abhinav Bhatele <bhatele@cs.umd.edu>
2023-03-05 00:24:45 -0500

    update copyright year to 2023

--- hatchet/readers/apex_reader.py
+++ hatchet/readers/apex_reader.py
@@ -1,5 +1,5 @@
-# Copyright 2022 University of Oregon and other Hatchet Project Developers. See
-# the top-level LICENSE file for details.
+# Copyright 2022-2023 University of Oregon and other Hatchet Project
+# Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
 
