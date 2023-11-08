5339c81f2ddb030921fd2e6616dbbe65a7a0bdcb
CarterLUMD <carterl2717@gmail.com>
2023-11-08 15:24:30 -0500

    updates to console.py

--- hatchet/_config/__init__.py
+++ hatchet/_config/__init__.py
@@ -1,7 +1,1 @@
-# __all__ = ["get_option", "set_option", "reset_option"]
-# from hatchet._config import config
-# from hatchet.hatchet._config.config import (
-#     get_option,
-#     reset_option,
-#     set_option,
-# )
+# __init__
