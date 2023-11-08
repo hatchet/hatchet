08e02fd8670c30dcf572d15b79147299b855c381
CarterLUMD <carterl2717@gmail.com>
2023-11-08 15:24:30 -0500

    updates to console.py

--- hatchet/_config/config.py
+++ hatchet/_config/config.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
 
+import sys
+import traceback
+
+from collections import defaultdict
+
+from ..util.colormaps import ColorMaps
 from optparse import OptionError
 from typing import (
     TYPE_CHECKING,
@@ -8,106 +14,142 @@ from typing import (
 import warnings
 
 
-class GlobalConfig:
-    # holds registered option metadata
-    # registered_options: dict[str, RegisteredOption] = {}
-    # global_config: dict[str, Any] = {}
-    registered_options: dict[str, Any] = {
-        "colormap": "RdYlGn",
-        "invert_colormap": False,
-        "depth": 10000,
-    }
+# class GlobalConfig:
+# holds registered option default data
+registered_options: dict[str, Any] = {
+    "colormap": "RdYlGn",
+    "invert_colormap": False,
+    "depth": 10000,
+}
 
-    # holds the current values for registered options
-    # global_config: dict[str, Any] = {}
-    global_config: dict[str, Any] = {
-        "colormap": "RdYlGn",
-        "invert_colormap": False,
-        "depth": 10000,
-    }
+# holds the current values for registered options
+global_config: dict[str, Any] = {
+    "colormap": "RdYlGn",
+    "invert_colormap": False,
+    "depth": 10000,
+}
 
-    # In these dicts we want colormap, invertcolormap, depth
-    # registered_options = {"colormap": "RdYlGn", "invert_colormap": False, "depth": 10000}
-    # global_config = registered_options
+colormaps = [
+    "RdYlGn",
+    "BrBG",
+    "PiYG",
+    "PRGn",
+    "PuOr",
+    "RdBu",
+    "RdGy",
+    "RdYlBu",
+    "Spectral",
+]
 
-    def get_option(self, key: str) -> Any:
-        if len(key) == 0:  # Or key is invalid
-            raise OptionError("No such keys(s)")
-        else:
-            return self.global_config[key]
 
-    def reset_option(self, key: str, silent: bool = False) -> None:
-        if len(key) == 0:  # Or key is invalid
-            raise OptionError("No such keys(s)")
-        # Need to check if its "all" or a specific key
-        if key == "all":
-            global_config = self.registered_options
-        elif self.registered_options.has_key(key):
-            # If it's a specific key and its valid
-            global_config[key] = self.registered_options[key]
-        else:
-            raise ValueError(
-                "You must specify a valid key. Or, use the special keyword "
-                '"all" to reset all the options to their default value'
-            )
+# This function returns the current value of the specified key
+def get_option(key: str) -> Any:
+    if len(key) == 0 or key not in registered_options:
+        raise ValueError("No such keys(s)")
+    else:
+        return global_config[key]
 
-    def set_option(self, key: str, val: Any) -> None:
-        if len(key) == 0:  # Or key is invalid
-            raise OptionError("No such keys(s)")
-        # Also need to check if val is valid for that specific key
-        # Some keys take strings, others take bools, others take ints
-        # Do we need bounds for some of the values? (Ex. Can only be between 1 and 500)
 
-        # set_validators(key, val)
+# This function updates the value of the specified key in the global_config dictionary.
+def set_option(key: str, val: Any):
+    if len(key) == 0 or key not in registered_options:
+        raise ValueError("No such keys(s)")
+    # Also need to check if val is valid for that specific key. Some keys take strings, others take bools, others take ints
+    if set_validators(key, val):
         # If its valid, update the key, value pair
-        self.global_config[key] = val
+        global_config[key] = val
+    # If its not valid, an error will be thrown by one of the validator functions
 
-    def set_validators(self, key, value):
-        if key == "colormap":
-            return self.str_validator(key, value)
-        elif key == "invert_colormap":
-            return self.bool_validator(key, value)
-        elif key == "depth":
-            return self.int_validator(key, value)
-        else:
-            raise OptionError("No such keys(s)")
 
-    def bool_validator(key, value):
-        if not isinstance(value, bool):
-            raise TypeError(
-                'Error loading configuration: The Value "{}" for Configuration "{}" must be of type bool'.format(
-                    value, key
-                )
+# This function resets the value specified key back to its default value.
+# If 'all' is passed in, it resets the values of all keys.
+def reset_option(key: str) -> None:
+    if len(key) == 0:
+        raise OptionError("No such keys(s)")
+    # Need to check if its "all" or a specific key
+    if key in registered_options:
+        # If it's a specific key and its valid
+        global_config[key] = registered_options[key]
+    elif key == "all":
+        for k, v in registered_options.items():
+            global_config[k] = v
+    else:
+        raise ValueError(
+            "You must specify a valid key. Or, use the special keyword "
+            '"all" to reset all the options to their default value'
+        )
+
+
+# Function to check the specifified key enetered. It sends the value to the corresponding validator depending on key enetered.
+def set_validators(key, value):
+    if key == "colormap":
+        # return str_validator(key, value)
+        if str_validator(key, value):
+            return is_valid_colormap(key, value)
+    elif key == "invert_colormap":
+        return bool_validator(key, value)
+    elif key == "depth":
+        return int_validator(key, value)
+    else:
+        raise ValueError("No such keys(s)")
+
+
+# Validator to check if the value entered is of type bool
+def bool_validator(key, value):
+    if type(value) is not bool:
+        raise TypeError(
+            'Error loading configuration: The Value "{}" for Configuration "{}" must be of type Bool'.format(
+                value, key
             )
-        else:
-            return True
+        )
+    else:
+        return True
 
-    def str_validator(key, value):
-        if not isinstance(value, str):
-            raise TypeError(
-                'Error loading configuration: The Value "{}" for Configuration "{}" must be of type string'.format(
-                    value, key
-                )
-            )
-        else:
-            return True
 
-    def int_validator(key, value):
-        if not isinstance(value, int):
-            raise TypeError(
-                'Error loading configuration: The Value "{}" for Configuration "{}" must be of type int'.format(
-                    value, key
-                )
+# Validator to check if the value entered is of type string
+def str_validator(key, value):
+    if type(value) is not str:
+        raise TypeError(
+            'Error loading configuration: The Value "{}" for Configuration "{}" must be of type string'.format(
+                value, key
             )
-        else:
-            return True
+        )
+    else:
+        return True
 
-    def float_validator(key, value):
-        if not isinstance(value, float):
-            raise TypeError(
-                'Error loading configuration: The Value "{}" for Configuration "{}" must be of type float'.format(
-                    value, key
-                )
+
+# Validator to check if the value entered is of type int
+def int_validator(key, value):
+    if type(value) is not int:
+        raise TypeError(
+            'Error loading configuration: The Value "{}" for Configuration "{}" must be of type int'.format(
+                value, key
             )
-        else:
-            return True
+        )
+    if key == "depth" and value < 1:
+        raise ValueError("Depth must be greater than 1")
+    return True
+
+
+# Validator to check if the value entered is of type float
+def float_validator(key, value):
+    if type(value) is not float:
+        raise TypeError(
+            'Error loading configuration: The Value "{}" for Configuration "{}" must be of type float'.format(
+                value, key
+            )
+        )
+    else:
+        return True
+
+
+# Validator to check if the colormap specfied exists and is valid
+def is_valid_colormap(key, colormap_name):
+    if colormap_name in colormaps:
+        return True
+    else:
+        raise ValueError(
+            'Error setting ColorMap: The value "{}" for Configuration "{}" is not a valid colormap'.format(
+                colormap_name, key
+            )
+        )
