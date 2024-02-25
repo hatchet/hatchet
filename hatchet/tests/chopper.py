8e2116a1f712d36451ad336b1c2d061fb07ad00d
ocnkr <ocankur@umd.edu>
2024-02-25 16:11:53 -0500

    check if the load_imbalance cause hanging

--- hatchet/tests/chopper.py
+++ hatchet/tests/chopper.py
@@ -40,8 +40,8 @@ def test_flat_profile(calc_pi_hpct_db):
 def test_load_imbalance(calc_pi_hpct_db):
     """Validate that the load imbalance is calculated correctly."""
 
-    # graphframe = GraphFrame.from_hpctoolkit(str(calc_pi_hpct_db))
-    # load_imb_gf = graphframe.load_imbalance(metric_column="time (inc)", threshold=0.01)
+    graphframe = GraphFrame.from_hpctoolkit(str(calc_pi_hpct_db))
+    load_imb_gf = graphframe.load_imbalance(metric_column="time (inc)", threshold=0.01)
 
     # # Check if load imbalance is correct for the root node.
     # root = graphframe.graph.roots[0]
