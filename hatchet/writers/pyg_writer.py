0c8219ced2c784b6feec0b8b08f41abecfe38e55
ocnkr <ocankur@umd.edu>
2025-02-07 14:32:57 -0500

    update copyright

--- hatchet/writers/pyg_writer.py
+++ hatchet/writers/pyg_writer.py
@@ -1,5 +1,5 @@
-# Copyright 2017-2024 Lawrence Livermore National Security, LLC and other
-# Hatchet Project Developers. See the top-level LICENSE file for details.
+# Copyright 2021-2024 University of Maryland and other Hatchet Project
+# Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
 
