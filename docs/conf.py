38f8b41f8fe226ef6556577b36b9b76fedf78bca
Abhinav Bhatele <bhatele@cs.umd.edu>
2025-10-22 13:39:16 -0400

    release: update version number to 1.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = "Abhinav Bhatele"
 # The short X.Y version
 version = "1.4.1"
 # The full version, including alpha/beta/rc tags
-release = "1.4.1rc3"
+release = "1.4.1"
 
 
 # -- General configuration ---------------------------------------------------
--- hatchet/version.py
+++ hatchet/version.py
@@ -3,5 +3,5 @@
 #
 # SPDX-License-Identifier: MIT
 
-__version_info__ = ("1", "4", "1rc3")
+__version_info__ = ("1", "4", "1")
 __version__ = ".".join(__version_info__)
